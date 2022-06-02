4f01915beef1ed2409f6cc665aebcd38d6707b4f
Joel Robles <joel-robles-gasser@hotmail.ch>
2022-06-02 16:58:18 +0200

    added info to version file

---
+++ version.php
@@ -0,0 +1,31 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Plugin version and other meta-data are defined here.
+ *
+ * @package     rsync
+ * @copyright   2022, Joel Robles <joelgabriel.roblesgasser@students.bfh.ch> Vithursan Thayananthan <vithursan.thayananthan@students.bfh.ch>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'rsync'; # plugin name
+$plugin->release = '0.0.1'; # plugin version
+$plugin->version = 2022060200; # version number YYYYMMDDXX where XX is an incremental number
+$plugin->requires = 2021051700; # min moodle version, 3.11
+$plugin->maturity = MATURITY_ALPHA; # MATURITY_ALPHA, MATURITY_BETA, MATURITY_RC or MATURITY_STABLE
